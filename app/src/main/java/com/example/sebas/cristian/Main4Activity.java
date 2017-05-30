e5a1e8d5d4fc6d15fb6f57bec359f0d338a675d7
Cristian Arroyave <cdarroyaveg@uniremingtonmanizales.edu.co>
2017-05-30 15:46:35 -0500

    Prototipo terminado

--- app/src/main/java/com/example/sebas/cristian/Main4Activity.java
+++ app/src/main/java/com/example/sebas/cristian/Main4Activity.java
@@ -13,7 +13,7 @@ public class Main4Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
 
-        webView2 = (WebView)findViewById(R.id.moodles);
+        webView2 = (WebView)findViewById(R.id.correos);
         webView2.loadUrl("https://accounts.google.com/signin/v2/identifier?continue=http%3A%2F%2Fmail.google.com%2Fa%2Funiremington.edu.co%2F&ltmpl=default&service=mail&sacu=1&hd=uniremington.edu.co&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
     }
 }
